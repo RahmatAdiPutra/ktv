3e286e6040b194ece0175c0544bb539bd895269f
aman <rahmat.adi.putra.21@gmail.com>
2019-09-10 03:02:47 +0700

    project ktv

--- ktv-frontend-angular/src/environments/environment.prod.ts
+++ ktv-frontend-angular/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: '/ktv/server',
+  pusher: {
+    httpHost: window.location.hostname,
+    wsHost: window.location.hostname,
+    authEndpoint: '/ktv/server/broadcasting/auth',
+    key: '2c5db2342d2bbac9a70b',
+    cluster: 'ap1'
+  }
 };
