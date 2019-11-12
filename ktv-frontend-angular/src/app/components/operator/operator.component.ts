87873f46ebc885e83ac78080ba52c069affacfc6
rahmatadiputra <rahmat.adi.putra.21@gmail.com>
2019-11-12 22:27:48 +0700

    project ktv

--- ktv-frontend-angular/src/app/components/operator/operator.component.ts
+++ ktv-frontend-angular/src/app/components/operator/operator.component.ts
@@ -9,11 +9,19 @@ import { MatDialog } from '@angular/material';
 import { DialogDeleteComponent } from '../dialog-delete/dialog-delete.component';
 import { DialogAllowSoundCallComponent } from '../dialog-allow-sound-call/dialog-allow-sound-call.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-operator',
   templateUrl: './operator.component.html',
-  styleUrls: ['./operator.component.css']
+  styleUrls: ['./operator.component.css'],
+  animations: [
+    trigger('elementDetailPlaylist', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 
 export class OperatorComponent implements OnInit {
@@ -40,8 +48,9 @@ export class OperatorComponent implements OnInit {
     this.data.songs = [];
     this.data.rooms = [];
     this.data.calls = [];
-    this.data.playlists = [];
     this.data.audio = [];
+    this.data.playlists = [];
+    this.data.playlistCategory = [];
 
     this.data.song = {};
     this.data.room = {};
@@ -57,13 +66,15 @@ export class OperatorComponent implements OnInit {
 
     this.form.search = {};
 
-    this.column.playlists = ['action', 'title', 'artist', 'is_play'];
     this.column.songs = ['action', 'title', 'artist', 'genre', 'language'];
     this.column.rooms = ['name', 'status', 'guest_name', 'ip_address'];
     this.column.calls = ['action', 'name', 'guest'];
+    this.column.playlists = ['action', 'title', 'artist', 'is_play'];
+    this.column.playlistCategory = ['action', 'title', 'category', 'song'];
+    this.column.playlistDetail = ['title', 'artist', 'action'];
 
-    this.data.song.server = environment.hostVideo;
     this.data.song.url = '';
+    this.data.song.server = environment.hostVideo;
 
     this.data.audio = new Audio();
 
@@ -71,14 +82,35 @@ export class OperatorComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.operator.song().subscribe(res => this.song(res), error => console.log(error));
     this.operator.language().subscribe(res => this.language(res), error => console.log(error));
+    this.operator.category().subscribe(res => this.category(res), error => console.log(error));
+    this.operator.playlist().subscribe(res => this.playlist(res), error => console.log(error));
+    this.operator.song().subscribe(res => this.song(res), error => console.log(error));
     this.operator.room().subscribe(res => this.room(res), error => console.log(error));
     this.operator.call().subscribe(res => this.call(res), error => console.log(error));
   }
 
   language(res) {
     this.select.languages = res.payloads;
+  }
+
+  category(res) {
+    this.select.categories = res.payloads;
+    this.select.categories[this.select.categories.length] = {
+      id: 0,
+      name: 'All'
+    };
+    this.select.categories.sort((a, b) => {
+      const x = a.name.toLowerCase();
+      const y = b.name.toLowerCase();
+      return x < y ? -1 : x > y ? 1 : 0;
+    });
+  }
+
+  playlist(res) {
+    this.data.detailPlaylist =  res.payloads.data || null;
+    this.data.playlistCategory = res.payloads.data;
+    this.source.playlistCategory = new MatTableDataSource(this.data.playlistCategory);
   }
 
   song(res) {
@@ -95,9 +127,9 @@ export class OperatorComponent implements OnInit {
     this.data.rooms = res.payloads.data.filter(data => data.active_session !== null);
     this.roomSelectClear(this.data.rooms);
     this.source.rooms = new MatTableDataSource(this.data.rooms);
-    const allow = this.getAllowSound();
+    const allow = this.callGetAllowSound();
     if (!allow.length) {
-      this.setAllowSound(this.data.rooms);
+      this.callSetAllowSound(this.data.rooms);
     }
   }
 
@@ -133,18 +165,18 @@ export class OperatorComponent implements OnInit {
     this.page.song.length = res.payloads.total;
   }
 
+  songSelect(data) {
+    if (data) {
+      this.data.song.url = this.data.song.server + data.file_path;
+    }
+  }
+
   roomRefresh(res) {
     this.data.rooms = res.payloads.data;
     this.source.rooms = new MatTableDataSource(this.data.rooms);
     if (this.data.room.session) {
       this.roomSelect(this.data.rooms.find((v, k) => v.active_session_id === this.data.room.session));
     }
-  }
-
-  roomPlaylist(res) {
-    this.data.player.playing = res.payloads.room.player.playing;
-    this.data.playlists = res.payloads.room.playlist;
-    this.source.playlists = new MatTableDataSource(this.data.playlists);
   }
 
   roomSelect(data) {
@@ -157,61 +189,10 @@ export class OperatorComponent implements OnInit {
     this.data.room.token = data.token;
     this.data.room.key = data.activation_key;
     if (this.data.room.session) {
-      this.operator.playlist(this.data.room.key).subscribe(res => this.roomPlaylist(res), error => console.log(error));
+      this.operator.roomPlaylist(this.data.room.key).subscribe(res => this.roomPlaylist(res), error => console.log(error));
       this.channelRoomPlaylist(this.data.room);
     } else {
       this.openToast('Room session not active', '');
-    }
-  }
-
-  callSelect(data) {
-    this.operator.callRespond(data.room.activation_key).subscribe(res => res, error => console.log(error));
-    this.roomSelect(this.data.rooms.find((v, k) => v.id === data.room_id));
-  }
-
-  songSelect(data) {
-    if (data) {
-      this.data.song.url = this.data.song.server + data.file_path;
-    }
-  }
-
-  songPlaylistSelect(data) {
-    if (this.data.room.session) {
-      this.data.room.songId = data.id;
-      this.operator.playing(this.data.room).subscribe(
-        res => this.openToast('Song ' + this.data.player.playing, ''),
-        error => console.log(error)
-      );
-    }
-  }
-
-  playToggle() {
-    if (this.data.room.session) {
-      if (this.data.playlists.length) {
-        this.operator.playToggle(this.data.room.key).subscribe(
-          res => this.openToast('Song ' + this.data.player.playing, ''),
-          error => console.log(error)
-        );
-      } else {
-        this.openToast('There are no songs in the playlist', '');
-      }
-    } else {
-      this.openToast('There are no songs in the playlist', '');
-    }
-  }
-
-  vocalToggle() {
-    if (this.data.room.session) {
-      if (this.data.playlists.length) {
-        this.operator.vocalToggle(this.data.room.key).subscribe(
-          res => this.data.player.vocal ? this.openToast('Vocal off', '') : this.openToast('Vocal on', ''),
-          error => console.log(error)
-        );
-      } else {
-        this.openToast('There are no songs in the playlist', '');
-      }
-    } else {
-      this.openToast('There are no songs in the playlist', '');
     }
   }
 
@@ -227,7 +208,23 @@ export class OperatorComponent implements OnInit {
     });
   }
 
-  playlistDrop(event: CdkDragDrop < string[] > ) {
+  roomPlaylist(res) {
+    this.data.player.playing = res.payloads.room.player.playing;
+    this.data.playlists = res.payloads.room.playlist;
+    this.source.playlists = new MatTableDataSource(this.data.playlists);
+  }
+
+  roomPlaylistSelectSong(data) {
+    if (this.data.room.session) {
+      this.data.room.songId = data.id;
+      this.operator.roomPlaylistPlaying(this.data.room).subscribe(
+        res => this.openToast('Song ' + this.data.player.playing, ''),
+        error => console.log(error)
+      );
+    }
+  }
+
+  roomPlaylistDrop(event: CdkDragDrop < string[] > ) {
     const prevIndex = event.item.data;
     moveItemInArray(this.data.playlists, prevIndex, event.currentIndex);
     this.table.renderRows();
@@ -236,13 +233,13 @@ export class OperatorComponent implements OnInit {
     this.data.playlists.forEach((v) => {
       this.data.room.dataList.push(v.id);
     });
-    this.operator.playlistSongReorder(this.data.room).subscribe(res => res, error => console.log(error));
+    this.operator.roomPlaylistReorderSong(this.data.room).subscribe(res => res, error => console.log(error));
   }
 
-  addSongToPlaylist(data) {
+  roomPlaylistAddSong(data) {
     if (this.data.room.session) {
       this.data.room.songId = data.id;
-      this.operator.playlistSongAdd(this.data.room).subscribe(
+      this.operator.roomPlaylistAddSong(this.data.room).subscribe(
         res => this.openToast('Song added to playlist', ''),
         error => console.log(error)
       );
@@ -251,14 +248,33 @@ export class OperatorComponent implements OnInit {
     }
   }
 
-  deleteAllSongFromPlaylist() {
+  roomPlaylistAddAllSong(data) {
+    console.log(data);
+    if (this.data.room.session) {
+      data.forEach((v) => {
+        this.roomPlaylistAddSong(v);
+      });
+    } else {
+      this.openToast('Room session not active', '');
+    }
+  }
+
+  roomPlaylistDeleteSong(data) {
+    this.data.room.songListId = data.id;
+    this.operator.roomPlaylistDeleteSong(this.data.room).subscribe(
+      res => this.openToast('Song deleted from playlist', ''),
+      error => console.log(error)
+    );
+  }
+
+  roomPlaylistDeleteAllSong() {
     if (this.data.room.session) {
       if (this.data.playlists.length) {
         const dialogRef = this.dialog.open(DialogDeleteComponent);
         dialogRef.afterClosed().subscribe(result => {
           if (result === true) {
             this.data.playlists.forEach((v) => {
-              this.deleteSongFromPlaylist(v);
+              this.roomPlaylistDeleteSong(v);
             });
           }
         });
@@ -270,12 +286,129 @@ export class OperatorComponent implements OnInit {
     }
   }
 
-  deleteSongFromPlaylist(data) {
-    this.data.room.songListId = data.id;
-    this.operator.playlistSongDelete(this.data.room).subscribe(
-      res => this.openToast('Song deleted from playlist', ''),
-      error => console.log(error)
-    );
+  roomPlaylistTogglePlay() {
+    if (this.data.room.session) {
+      if (this.data.playlists.length) {
+        this.operator.roomPlaylistTogglePlay(this.data.room.key).subscribe(
+          res => this.openToast('Song ' + this.data.player.playing, ''),
+          error => console.log(error)
+        );
+      } else {
+        this.openToast('There are no songs in the playlist', '');
+      }
+    } else {
+      this.openToast('There are no songs in the playlist', '');
+    }
+  }
+
+  roomPlaylistToggleVocal() {
+    if (this.data.room.session) {
+      if (this.data.playlists.length) {
+        this.operator.roomPlaylistToggleVocal(this.data.room.key).subscribe(
+          res => this.data.player.vocal ? this.openToast('Vocal off', '') : this.openToast('Vocal on', ''),
+          error => console.log(error)
+        );
+      } else {
+        this.openToast('There are no songs in the playlist', '');
+      }
+    } else {
+      this.openToast('There are no songs in the playlist', '');
+    }
+  }
+
+  callRespond(data) {
+    this.operator.callRespond(data.room.activation_key).subscribe(res => res, error => console.log(error));
+    this.roomSelect(this.data.rooms.find((v, k) => v.id === data.room_id));
+  }
+
+  callToggleFab() {
+    const rooms = this.data.rooms;
+    const allow = JSON.parse(localStorage.getItem('allow'));
+    rooms.forEach((v, k) => {
+      const index = allow.map(e => e.name).indexOf(v.name);
+      if (index >= 0) {
+        rooms[k].allow = allow[index].allow;
+      } else {
+        rooms[k].allow = true;
+      }
+    });
+    const dialogRef = this.dialog.open(DialogAllowSoundCallComponent, {
+      data: rooms
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (typeof result !== 'undefined') {
+        this.callSetAllowSound(result);
+      }
+    });
+  }
+
+  callSetAllowSound(data) {
+    const allow = [];
+    data.forEach((v, k) => {
+      allow.push({
+        name: v.name,
+        allow: typeof v.allow === 'undefined' ? true : v.allow
+      });
+    });
+    localStorage.setItem('allow', JSON.stringify(allow));
+  }
+
+  callGetAllowSound() {
+    const allow = [];
+    if (localStorage.getItem('allow')) {
+      JSON.parse(localStorage.getItem('allow')).forEach((v, k) => {
+        if (v.allow) {
+          allow.push(v.name);
+        }
+      });
+    }
+    return allow;
+  }
+
+  callSound(res) {
+    if (res) {
+      this.data.audio.src = environment.hostVideo + 'call-operator.mp3';
+      this.data.audio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+      }, false);
+      this.data.audio.autoplay = true;
+    } else {
+      if (this.data.calls.length <= 1) {
+        this.data.audio.src = '';
+      }
+    }
+  }
+
+  formSearch(data?) {
+    if (data) {
+      this.songPaginator.pageIndex = 0;
+      if (this.form.search) {
+        this.operator.search(this.form.search, 1).subscribe(
+          res => this.songSearch(res),
+          error => console.log(error)
+        );
+      }
+    } else {
+      if (this.form.search.playlist || (this.form.search.category >= 0)) {
+        this.operator.playlist(this.form.search).subscribe(res => this.playlist(res), error => console.log(error));
+      }
+    }
+  }
+
+  formSearchClear(data?) {
+    if (data) {
+      this.songPaginator.pageIndex = 0;
+      this.form.search.song = '';
+      this.form.search.artist = '';
+      this.form.search.language = '';
+      this.form.search.new = false;
+      this.operator.song().subscribe(res => this.songRefresh(res), error => console.log(error));
+    } else {
+      this.form.search.playlist = '';
+      this.form.search.category = '';
+      this.operator.playlist().subscribe(res => this.playlist(res), error => console.log(error));
+    }
   }
 
   channelRoomCall() {
@@ -283,13 +416,13 @@ export class OperatorComponent implements OnInit {
       .listen('.respond.operator', message => {
         this.operator.call().subscribe(res => this.call(res), error => console.log(error));
         this.openToast(message.message, '');
-        this.sound(false);
+        this.callSound(false);
       }).listen('.call.operator', message => {
-        const allow = this.getAllowSound();
+        const allow = this.callGetAllowSound();
         this.operator.call().subscribe(res => this.call(res), error => console.log(error));
         this.openToast(message.message, '');
         if (allow.indexOf(message.message.slice(0, -17)) >= 0) {
-          this.sound(true);
+          this.callSound(true);
         }
       });
   }
@@ -319,78 +452,10 @@ export class OperatorComponent implements OnInit {
     window.Echo.leave(`session.${this.data.room.token}`);
   }
 
-  formSearch() {
-    this.songPaginator.pageIndex = 0;
-    if (this.form.search) {
-      this.operator.search(this.form.search, 1).subscribe(
-        res => this.songSearch(res),
-        error => console.log(error)
-      );
-    }
-  }
-
-  formSearchClear() {
-    this.songPaginator.pageIndex = 0;
-    this.form.search.song = '';
-    this.form.search.artist = '';
-    this.form.search.language = '';
-    this.form.search.new = false;
-    this.operator.song().subscribe(res => this.songRefresh(res), error => console.log(error));
-  }
-
   openToast(message: string, action: string) {
     this.toast.open(message, action, {
       duration: 2000,
     });
-  }
-
-  onToggleFab() {
-    const dialogRef = this.dialog.open(DialogAllowSoundCallComponent, {
-      data: JSON.parse(localStorage.getItem('allow'))
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (typeof result !== 'undefined') {
-        this.setAllowSound(result);
-      }
-    });
-  }
-
-  setAllowSound(data) {
-    const allow = [];
-    data.forEach((v, k) => {
-      allow.push({
-        name: v.name,
-        allow: typeof v.allow === 'undefined' ? true : v.allow
-      });
-    });
-    localStorage.setItem('allow', JSON.stringify(allow));
-  }
-
-  getAllowSound() {
-    const allow = [];
-    if (localStorage.getItem('allow')) {
-      JSON.parse(localStorage.getItem('allow')).forEach((v, k) => {
-        if (v.allow) {
-          allow.push(v.name);
-        }
-      });
-    }
-    return allow;
-  }
-
-  sound(res) {
-    if (res) {
-      this.data.audio.src = environment.hostVideo + 'call-operator.mp3';
-      this.data.audio.addEventListener('ended', function() {
-        this.currentTime = 0;
-        this.play();
-      }, false);
-      this.data.audio.autoplay = true;
-    } else {
-      if (this.data.calls.length <= 1) {
-        this.data.audio.src = '';
-      }
-    }
   }
 
 }
--- ktv-frontend-angular/src/app/services/operator.service.ts
+++ ktv-frontend-angular/src/app/services/operator.service.ts
@@ -13,28 +13,6 @@ export class OperatorService {
 
   constructor(private http: HttpClient) { }
 
-  room() {
-    return this.http.get(`${this.env.apiUrl}/api/operator/room`).pipe(catchError(this.errorHandler));
-  }
-
-  song(page?) {
-    let params = new HttpParams();
-
-    if (page !== undefined && page !== '') {
-      params = params.append('page', page);
-    }
-
-    return this.http.get(`${this.env.apiUrl}/api/operator/browse`, { params }).pipe(catchError(this.errorHandler));
-  }
-
-  call() {
-    return this.http.get(`${this.env.apiUrl}/api/operator/call`).pipe(catchError(this.errorHandler));
-  }
-
-  language() {
-    return this.http.get(`${this.env.apiUrl}/api/operator/lang`).pipe(catchError(this.errorHandler));
-  }
-
   search(query, page?) {
     let params = new HttpParams();
 
@@ -69,49 +47,97 @@ export class OperatorService {
       .get(`${this.env.apiUrl}/api/operator/search`, {params}).pipe(catchError(this.errorHandler));
   }
 
-  playlist(key) {
+  language() {
+    return this.http.get(`${this.env.apiUrl}/api/operator/lang`).pipe(catchError(this.errorHandler));
+  }
+
+  category() {
+    return this.http.get(`${this.env.apiUrl}/api/operator/playlist/category`).pipe(catchError(this.errorHandler));
+  }
+
+  playlist(data?) {
+    let params = new HttpParams();
+
+    if (data === undefined) {
+      data = {};
+      data.playlist = '';
+      data.category = '';
+    } else {
+      if (data.playlist === undefined) {
+        data.playlist = '';
+      }
+      if (data.category === undefined) {
+        data.category = '';
+      }
+    }
+
+    params = params.append('name', data.playlist);
+    params = params.append('playlist_category_id', data.category);
+
+    return this.http.get(`${this.env.apiUrl}/api/operator/playlist`, {params}).pipe(catchError(this.errorHandler));
+  }
+
+  song(page?) {
+    let params = new HttpParams();
+
+    if (page !== undefined && page !== '') {
+      params = params.append('page', page);
+    }
+
+    return this.http.get(`${this.env.apiUrl}/api/operator/browse`, { params }).pipe(catchError(this.errorHandler));
+  }
+
+  room() {
+    return this.http.get(`${this.env.apiUrl}/api/operator/room`).pipe(catchError(this.errorHandler));
+  }
+
+  call() {
+    return this.http.get(`${this.env.apiUrl}/api/operator/call`).pipe(catchError(this.errorHandler));
+  }
+
+  roomPlaylist(key) {
     return this.http.get(`${this.env.apiUrl}/api/controller/init?tv-token=${key}`).pipe(catchError(this.errorHandler));
   }
 
-  playToggle(key) {
+  roomPlaylistPlaying(data) {
+    return this.http.post(`${this.env.apiUrl}/api/controller/playlist/play-this-song?tv-token=${data.key}`, {
+       id: data.songId
+    }).pipe(catchError(this.errorHandler));
+  }
+
+  roomPlaylistReorderSong(data) {
+    return this.http.post(`${this.env.apiUrl}/api/controller/playlist/reorder?tv-token=${data.key}`, {
+      orders: data.dataList
+    }).pipe(catchError(this.errorHandler));
+  }
+
+  roomPlaylistAddSong(data) {
+    return this.http.post(`${this.env.apiUrl}/api/controller/playlist/add?tv-token=${data.key}`, {
+      id: data.songId
+    }).pipe(catchError(this.errorHandler));
+  }
+
+  roomPlaylistDeleteSong(data) {
+    return this.http.delete(`${this.env.apiUrl}/api/controller/playlist/delete-this-song?tv-token=${data.key}`, {
+      params: { id: data.songListId }
+    }).pipe(catchError(this.errorHandler));
+  }
+
+  roomPlaylistTogglePlay(key) {
     return this.http.post(`${this.env.apiUrl}/api/controller/control/play-toggle?tv-token=${key}`, {
       ok: 1
     }).pipe(catchError(this.errorHandler));
   }
 
-  vocalToggle(key) {
+  roomPlaylistToggleVocal(key) {
     return this.http.post(`${this.env.apiUrl}/api/controller/control/vocal?tv-token=${key}`, {
       ok: 1
-    }).pipe(catchError(this.errorHandler));
-  }
-
-  playing(data) {
-    return this.http.post(`${this.env.apiUrl}/api/controller/playlist/play-this-song?tv-token=${data.key}`, {
-       id: data.songId
     }).pipe(catchError(this.errorHandler));
   }
 
   callRespond(key) {
     return this.http.post(`${this.env.apiUrl}/api/controller/call/respond?tv-token=${key}`, {
       who: 'operator'
-    }).pipe(catchError(this.errorHandler));
-  }
-
-  playlistSongReorder(data) {
-    return this.http.post(`${this.env.apiUrl}/api/controller/playlist/reorder?tv-token=${data.key}`, {
-      orders: data.dataList
-    }).pipe(catchError(this.errorHandler));
-  }
-
-  playlistSongAdd(data) {
-    return this.http.post(`${this.env.apiUrl}/api/controller/playlist/add?tv-token=${data.key}`, {
-      id: data.songId
-    }).pipe(catchError(this.errorHandler));
-  }
-
-  playlistSongDelete(data) {
-    return this.http.delete(`${this.env.apiUrl}/api/controller/playlist/delete-this-song?tv-token=${data.key}`, {
-      params: { id: data.songListId }
     }).pipe(catchError(this.errorHandler));
   }
 
